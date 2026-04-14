8682cedf9c1aaa45a1a2d8c89a2f49fcbcc407d0
EthanY <zexiyang1997@gmail.com>
2026-04-14 00:17:07 -0400

    Updates

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -1,16 +1,20 @@
+
 import requests
 import json
-
-import os
-from dotenv import load_dotenv
 from datetime import date
 
-load_dotenv(dotenv_path=".env")
+# import os
+# from dotenv import load_dotenv
+# load_dotenv(dotenv_path=".env")
 
-API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = "MrBeast"
+from airflow.decorators import task
+from airflow.models import Variable
+
+API_KEY = Variable.get("API_KEY")
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE", "MrBeast")
 maxResults = 50
 
+@task
 def get_playlist_id():
     try:
 
@@ -33,6 +37,7 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
 
+@task
 def get_video_ids(playlist_id):
 
     video_ids = []
@@ -68,7 +73,7 @@ def get_video_ids(playlist_id):
     
 
 
-
+@task
 def extract_video_data(video_ids):
     video_data = []
 
@@ -90,10 +95,11 @@ def extract_video_data(video_ids):
                 video_info = {
                     'video_id': item['id'],                        
                         'title': item['snippet']['title'],
-                        'published_at': item['snippet']['publishedAt'],
-                        'view_count': item['statistics'].get('viewCount', None),
-                        'like_count': item['statistics'].get('likeCount', None),
-                        'comment_count': item['statistics'].get('commentCount', None)
+                        'publishedAt': item['snippet']['publishedAt'],
+                        "duration": item['contentDetails']['duration'],
+                        'viewCount': item['statistics'].get('viewCount', None),
+                        'likeCount': item['statistics'].get('likeCount', None),
+                        'commentCount': item['statistics'].get('commentCount', None)
                     }
                 video_data.append(video_info)
 
@@ -102,8 +108,9 @@ def extract_video_data(video_ids):
     
     return video_data
 
+@task
 def save_to_json(data):
-    file_path = f"/home/yangzx_97/YouTube_ETL/data/data_{date.today()}.json"
+    file_path = f"./data/YT_data_{date.today()}.json"
     with open(file_path, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
